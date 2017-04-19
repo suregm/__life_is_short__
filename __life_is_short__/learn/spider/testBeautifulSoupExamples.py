c7c0ac43f89e7ddc8e8b011b8d7ed4d11e327628
suregm <sure0kuaile@qq.com>
2017-04-19 08:18:25 +0800

    learn spider. information markup. example

--- __life_is_short__/learn/spider/testBeautifulSoupExamples.py
+++ __life_is_short__/learn/spider/testBeautifulSoupExamples.py
@@ -5,3 +5,25 @@ from bs4 import BeautifulSoup
 # 定向爬取，固定url
 # 可行性：网页静态文本形式，查看robots.txt
 
+def getHTMLText(url):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return ""
+    return ""
+
+def fillUnivList(ulist, html):
+    pass
+
+def printUnivList(ulist, num):
+    print("Suc" + str(num))
+
+def main():
+    uinfo = []
+    url = 'http://www.zuihaodaxue.cn/zuihaodaxuepaiming2017.html'
+    html = getHTMLText(url)
+    fillUnivList(uinfo, html)
+    printUnivList(uinfo, 50)    # 前50名
